f55c9a6e5f5d49ac7743ab275c4ab14b39754be2
Thomas Kragerud <thomas.kragerud@gmail.com>
2023-06-01 19:35:54 +0200

    Working eksm2022

--- Ada/eksam2022.adb
+++ Ada/eksam2022.adb
@@ -13,15 +13,84 @@
 
 
 -- Specification (declarations)
+with Ada.Text_IO; use Ada.Text_IO;
 
-procedure Main is
--- Define the paramaters 
-    task Worker is
+procedure Eksam2022 is
+    -- Define the array i need 
+    type Bool_Array is array (Integer range <>) of Boolean;
+
+
+
+
+    -- Define the protecte Object containing an array and the 
+    -- Funcions needed to work on it 
+    protected Acceptance_List is
+        procedure I_COMPLETED(ID : Integer);
+        function Status return Boolean;
+        entry Finished_Rendevous;
+    private
+        count : Integer := 0;
+        Status_Array : Bool_Array(1 .. 3) := (others => False);
+
+
+    end Acceptance_List;
+
+    protected body Acceptance_List is
+        procedure I_COMPLETED(ID : Integer) is
+        begin
+            Count := Count + 1;
+            Status_Array(ID) := True;
+        end I_COMPLETED; 
+
+        function Status return Boolean is
+        All_True : Boolean := True; -- Initialize as True
+        begin
+            for I in Status_Array'range loop
+                -- AND each element with the current result
+                All_True := All_True and Status_Array(I);
+            end loop;
+            return All_True;
+        end Status;
+
+        entry finished_Rendevous when count >= 3 is
+        begin
+            null;
+        end finished_Rendevous;
+    end Acceptance_List;
+    
+    task type Worker (ID : Integer) is
         entry Start;
-    
-    task Clinet is
-        entry Kjor;
-    
+    end Worker;
+
+    task body Worker is
+    begin
+        accept Start do
+            null; 
+        end Start;
+         -- Notify that task is finished
+        Acceptance_List.I_COMPLETED(ID);
+        Acceptance_List.Finished_Rendevous;
+        Put_Line("Worker" & Integer'Image(ID) & "Finished");
+    end Worker;
+
+    -- Declare the variable Stat
+    Stat : Boolean;
+
+    -- Instatiate workers 
+    Worker1 : Worker(1);
+    Worker2 : Worker(2);
+    Worker3 : Worker(3);
 begin
--- Run 
-end Main;
+    -- Start the workers
+    Worker1.Start;
+    Worker2.Start;
+    Worker3.Start;
+
+        -- Wait until all workers have completed their tasks
+    
+     -- Get the status
+    Acceptance_List.Finished_Rendevous;
+    Stat := Acceptance_List.Status;
+    -- Convert boolean to string
+    Ada.Text_IO.Put_Line("Status: " & Boolean'Image(Stat));
+end Eksam2022;
--- Ada/hello_world.adb
+++ Ada/hello_world.adb
@@ -4,3 +4,13 @@ procedure Hello_World is
 begin
     Ada.Text_IO.Put_Line("Hello World");
 end Hello_World;
+
+
+task Worker is
+        entry Start;
+    end Worker; 
+    
+    task Clinet is
+        entry Kjor;
+    end Clinet;
+    
